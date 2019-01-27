67e546a18c65f954f817afb7d7d08c66b8ba9280
diaaanek <dkorongy@gmail.com>
2019-01-26 22:45:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// always use const and let to declare variables.
+
+customerName = 'bob';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+//
+// overwriteBestCustomer()
+// overwrites the best customer
+
+function overwriteBestCustomer(){
+
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Diana';
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'diane';
+}
